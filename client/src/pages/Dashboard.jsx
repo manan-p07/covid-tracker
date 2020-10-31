0df88c2c477c90d50351ab28723148b7a93b2ae3
Manan Patel <manan07@Manans-MBP.home>
2020-10-31 16:33:46 -0400

    Backend works

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -4,11 +4,13 @@ import axios from "axios";
 class Dashboard extends Component {
     constructor(props) {
         super(props);
+
         this.state = {
             USState: "NJ",
             currentCases: 0,
             message: ""
         }
+
         this.getCovidCases = this.getCovidCases.bind(this);
     }
     
@@ -17,13 +19,15 @@ class Dashboard extends Component {
             state: this.state.USState
         }
         axios.post("http://localhost:5000/get-current-cases", data).then(res => {
-            this.setState({message: res.message});
+            console.log(res.data)
+            this.setState({message: res.data.currentCases});
         })
     }
 
     render() {
         return (
             <>
+                <h1>Title</h1>
                 <button onClick={this.getCovidCases}>Get Covid Cases</button>
                 <h1>{this.state.message}</h1>
             </>
